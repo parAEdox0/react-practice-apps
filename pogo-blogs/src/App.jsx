0a5335249b18efc5c36c387e3ca63310eab66300
Parth Ekbote <paraedox0@gmail.com>
2025-09-25 23:21:55 +0530

    Conditional Loading, Error rendering.

--- pogo-blogs/src/App.jsx
+++ pogo-blogs/src/App.jsx
@@ -4,15 +4,34 @@ import BlogList from "../components/BlogList";
 
 const App = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
-    fetch("http://localhost:5000/blogs")
-      .then((res) => res.json())
-      .then((data) => setBlogs(data));
+    setTimeout(() => {
+      fetch("http://localhost:5000/blogs")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Could not find the resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsLoading(false);
+          setError(false);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setIsLoading(false);
+        });
+    }, 1500);
   }, []);
 
   return (
     <>
       <Navbar />
+      {error && <p className="ml-[200px]">{error}</p>}
+      {isLoading && <p className="ml-[200px]">Loading...</p>}
       {blogs && <BlogList blogs={blogs} />}
     </>
   );
